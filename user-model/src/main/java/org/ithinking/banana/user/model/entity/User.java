7657111eeb3195d90d09d385cca06b4444e840a6
fucj <fucj@hyxt.com>
2016-05-17 14:34:57 +0800

    完善

--- user-model/src/main/java/org/ithinking/banana/user/model/entity/User.java
+++ user-model/src/main/java/org/ithinking/banana/user/model/entity/User.java
@@ -1,7 +1,65 @@
 package org.ithinking.banana.user.model.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * @author fuchujian
  */
+@Table(name = "user")
+@Entity
 public class User {
+    private Long userId;
+    private String nickName;
+    private String passwd;
+    private String mobile;
+    private String email;
+
+    @Id
+    @Column(name = "user_id")
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    @Column(name = "nick_name")
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    @Column(name = "passwd")
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+    @Column(name = "mobile")
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    @Column(name = "email")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }

8c4faf390904327e037aaefcb42d421d80d6264d
fucj <fucj@hyxt.com>
2016-05-18 10:58:32 +0800

    完善

---
+++ user-test/src/test/java/org/ithinking/banana/user/service/UserServiceTest.java
@@ -0,0 +1,33 @@
+package org.ithinking.banana.user.service;
+
+import org.ithinking.banana.user.api.Application;
+import org.ithinking.banana.user.model.entity.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author fuchujian
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    private static  Long userId = 1L;
+    private static  String loginId = "testLoginId";
+
+    @Test
+    public void testGetUserById(){
+        User user = userService.getUserById(userId);
+    }
+
+    @Test
+    public void testGetUserByLoginId(){
+        User user = userService.getUserByLoginId(loginId);
+    }
+}

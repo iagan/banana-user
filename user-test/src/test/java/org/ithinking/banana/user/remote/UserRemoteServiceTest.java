4fa40cea1e2baa1ebf1721559b2ccfa943295e00
fucj <fucj@hyxt.com>
2016-05-18 14:12:33 +0800

    完善

---
+++ user-test/src/test/java/org/ithinking/banana/user/remote/UserRemoteServiceTest.java
@@ -0,0 +1,13 @@
+package org.ithinking.banana.user.remote;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author fuchujian
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+public class UserRemoteServiceTest {
+}

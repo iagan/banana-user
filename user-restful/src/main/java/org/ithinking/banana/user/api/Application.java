a0da4cb579fd3eab7ad44fe4b0061c07bbdcd0f2
fucj <fucj@hyxt.com>
2016-05-18 10:46:50 +0800

    完善

--- user-restful/src/main/java/org/ithinking/banana/user/api/Application.java
+++ user-restful/src/main/java/org/ithinking/banana/user/api/Application.java
@@ -1,7 +1,23 @@
 package org.ithinking.banana.user.api;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 /**
  * @author fuchujian
  */
+@SpringBootApplication
+@ComponentScan(basePackages = "org.ithinking.banana")
+@EnableAutoConfiguration
 public class Application {
+    /**
+     * WebApplicationInitializer
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }

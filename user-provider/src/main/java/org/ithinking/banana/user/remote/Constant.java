daebaa266efaaa89b54d2c84fb32e7e20d4cfe33
fucj <fucj@hyxt.com>
2016-05-18 14:02:36 +0800

    完善

--- user-provider/src/main/java/org/ithinking/banana/user/remote/Constant.java
+++ user-provider/src/main/java/org/ithinking/banana/user/remote/Constant.java
@@ -7,5 +7,5 @@ package org.ithinking.banana.user.remote;
  * @date 2016-05-07
  */
 public interface Constant {
-    String DUBBO_REGISTRY = "banana-pay";
+    String DUBBO_REGISTRY = "banana-user";
 }
--- user-provider/src/main/java/org/ithinking/banana/user/remote/service/impl/UserRemoteServiceImpl.java
+++ user-provider/src/main/java/org/ithinking/banana/user/remote/service/impl/UserRemoteServiceImpl.java
@@ -1,9 +1,12 @@
 package org.ithinking.banana.user.remote.service.impl;
 
+import org.ithinking.banana.comm.BeanMapper;
+import org.ithinking.banana.user.model.entity.User;
 import org.ithinking.banana.user.remote.Constant;
 import org.ithinking.banana.user.remote.dto.UserDTO;
 import org.ithinking.banana.user.remote.service.UserRemoteService;
 import org.ithinking.banana.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * @author fuchujian
@@ -11,15 +14,26 @@ import org.ithinking.banana.user.service.UserService;
 @com.alibaba.dubbo.config.annotation.Service(interfaceClass = UserRemoteService.class, registry = Constant.DUBBO_REGISTRY)
 public class UserRemoteServiceImpl implements UserRemoteService {
 
+    @Autowired
     private UserService userService;
 
     @Override
     public UserDTO getUserById(Long userId) {
-        return null;
+        User user = userService.getUserById(userId);
+        UserDTO UserDTO = null;
+        if (user != null) {
+            UserDTO = BeanMapper.copyTo(user, UserDTO.class);
+        }
+        return UserDTO;
     }
 
     @Override
     public UserDTO getUserByLoginId(String loginId) {
-        return null;
+        User user = userService.getUserByLoginId(loginId);
+        UserDTO UserDTO = null;
+        if (user != null) {
+            UserDTO = BeanMapper.copyTo(user, UserDTO.class);
+        }
+        return UserDTO;
     }
 }

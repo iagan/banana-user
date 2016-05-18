918d7560774c1b3bcafba5477e63f3383e7159c4
fucj <fucj@hyxt.com>
2016-05-18 10:26:55 +0800

    完善

--- user-service/src/main/java/org/ithinking/banana/user/service/impl/UserServiceImpl.java
+++ user-service/src/main/java/org/ithinking/banana/user/service/impl/UserServiceImpl.java
@@ -1,21 +1,26 @@
 package org.ithinking.banana.user.service.impl;
 
+import org.ithinking.banana.user.mapper.UserMapper;
 import org.ithinking.banana.user.model.entity.User;
 import org.ithinking.banana.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
  * @author fuchujian
  */
 @Service
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserMapper userMapper;
+
     @Override
     public User getUserById(Long userId) {
-        return null;
+        return userMapper.getUserById(userId);
     }
 
     @Override
     public User getUserByLoginId(String loginId) {
-        return null;
+        return userMapper.getUserByLoginId(loginId);
     }
 }
